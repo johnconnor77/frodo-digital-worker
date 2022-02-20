0eef5e8d2f27fdbcda3c394a87566fcafb3dc34e
johnconnor77 <jfcalleh@unal.edu.co>
2022-02-20 00:46:22 -0500

    Process class which enhance the major capability of what Frodo does

---
+++ libraries/process.py
@@ -0,0 +1,30 @@
+from libraries.common import log_message, capture_page_screenshot, browser
+from libraries.google.google import Google
+from config import OUTPUT_FOLDER, tabs_dict
+
+
+class Process:
+    def __init__(self):
+        log_message("Initialization")
+
+        prefs = {
+            "profile.default_content_setting_values.notifications": 2,
+            "profile.default_content_settings.popups": 0,
+            "directory_upgrade": True,
+            "download.default_directory": OUTPUT_FOLDER,
+            "plugins.always_open_pdf_externally": True,
+            "download.prompt_for_download": False
+        }
+        browser.open_available_browser(preferences=prefs)
+        browser.set_window_size(1920, 1080)
+        browser.maximize_browser_window()
+
+    def start(self):
+        google = Google(browser, {"url": "https://www.google.com/ncr"})
+        tabs_dict["Google"] = len(tabs_dict)
+        google.access_google()
+        google.search_movie(movie_name="The lord of the Rings: The Return of the King itunes movie us")
+
+    def finish(self):
+        log_message("DW Process Finished")
+        browser.close_browser()
