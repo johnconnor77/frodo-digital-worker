bcdd1c7889024f2b961cd5265ec7ee7bb6d30dee
johnconnor77 <jfcalleh@unal.edu.co>
2022-02-21 18:43:09 -0500

    Added log messages for improve readibility of process

--- libraries/process.py
+++ libraries/process.py
@@ -21,14 +21,19 @@ class Process:
         browser.maximize_browser_window()
 
     def start(self):
+        log_message("Search Movie on Google")
         google = Google(browser, {"url": "https://www.google.com/ncr"})
         tabs_dict["Google"] = len(tabs_dict)
         google.access_google()
         google.search_movie(movie_name="The lord of the Rings: The Return of the King itunes movie us")
+
+        log_message("Extract information from Movie")
         itunes = Itunes(browser)
         del tabs_dict["Google"]
         tabs_dict["itunes"] = len(tabs_dict)
         itunes.extract_information()
+
+        log_message("Write Data as .xlsx on Output Folder")
         itunes.write_data_to_excel()
 
     def finish(self):
